7c7174868357e079783efc06e25ac217849c6099
Rakshit <102043646+Rak256@users.noreply.github.com>
2025-05-25 00:21:11 -0300

    Update theme-toggle.tsx
    Fixed theme toggle vertical spring up

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -42,14 +42,20 @@ export function ThemeToggle() {
       <div className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background bg-transparent ${
         isDark ? "justify-end" : "justify-start"
       }`}>
-      <motion.div 
-      className={` absolute h-5 w-5 bg-white rounded-full`}
-      layout
-      initial={false}
-      transition={{
-        layout:
-        {type:"spring", stiffness: 700, damping: 30},
-      }}/>
+      <motion.div
+        // don’t animate on mount or navigation
+        initial={false}
+        // manually animate horizontal movement rather than using layout. This ensures that the circle doesn't
+        //spring back up when the "/" path is taken through clicking the portfolio div
+        //Note: due to relative positioning, pixel offsets can be used
+        animate={{ x: isDark ? 24 : 0.5 }}
+        transition={{
+          type: 'spring',
+          stiffness: 700,
+          damping: 30,
+        }}
+        className="absolute top-[2px] left-0 h-5 w-5 rounded-full bg-white"
+      />
       </div>
     </button>
   )
